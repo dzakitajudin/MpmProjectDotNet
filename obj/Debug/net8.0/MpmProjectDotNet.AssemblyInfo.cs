1af8da19cf5638c4d5132fdd04b57a7e575520ca
Mochamad Dzaki Arief Tadjoedin <dzaki.tadjoedin@mpm-ho.com>
2024-05-30 11:29:56 +0700

    update sonar

--- obj/Debug/net8.0/MpmProjectDotNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/MpmProjectDotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MpmProjectDotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fb1d3649c03a59c5dcd3489be557d64982e435e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2080d0416abf3e6525e5168473d5f792b4e0f3b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MpmProjectDotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MpmProjectDotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
