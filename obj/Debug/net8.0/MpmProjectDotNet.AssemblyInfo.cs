96cba8ff075fbd76f03d47864fc2b89cc45080d2
Mochamad Dzaki Arief Tadjoedin <dzaki.tadjoedin@mpm-ho.com>
2024-06-12 11:20:09 +0700

    show secret token

--- obj/Debug/net8.0/MpmProjectDotNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/MpmProjectDotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MpmProjectDotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+147dfe897f5a90b0fb435ee1a7e76d702f1c0c8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa2b52581005d990a9983aa19fb2de8d5a91a157")]
 [assembly: System.Reflection.AssemblyProductAttribute("MpmProjectDotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MpmProjectDotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
